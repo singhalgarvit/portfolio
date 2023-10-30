beccede3fa59807964247f50ed5f74e47ab5c4f2
Garvit Singhal <garvitsinghal305@gmail.com>
2023-10-30 17:39:07 +0530

    asf

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB22Zti4wa0b70qOBMHP1re5lMIj7ccwwo",
-  authDomain: "personalportfolio-28315.firebaseapp.com",
-  databaseURL: "https://personalportfolio-28315-default-rtdb.firebaseio.com",
-  projectId: "personalportfolio-28315",
-  storageBucket: "personalportfolio-28315.appspot.com",
-  messagingSenderId: "572931778721",
-  appId: "1:572931778721:web:d43a48d33312e558aba8e0"
+  apiKey: "AIzaSyDDUGJWaTyNqeCpBbfqRDh2hVQ7Jri5nws",
+  authDomain: "portfolio-38607.firebaseapp.com",
+  databaseURL: "https://portfolio-38607-default-rtdb.firebaseio.com",
+  projectId: "portfolio-38607",
+  storageBucket: "portfolio-38607.appspot.com",
+  messagingSenderId: "403294971213",
+  appId: "1:403294971213:web:49321c5f5fba944dbcc304"
 };
 
 firebase.initializeApp(firebaseConfig);
