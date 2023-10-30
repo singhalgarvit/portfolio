06bd73fbb4ae2a7d57b8addc964f0661fe456ba2
Garvit Singhal <garvitsinghal305@gmail.com>
2023-10-30 17:29:52 +0530

    fisf

--- firebase.js
+++ firebase.js
@@ -1,32 +1,44 @@
-import {firebase} from "./firebase/app";
-import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB22Zti4wa0b70qOBMHP1re5lMIj7ccwwo",
-    authDomain: "personalportfolio-28315.firebaseapp.com",
-    databaseURL: "https://personalportfolio-28315-default-rtdb.firebaseio.com",
-    projectId: "personalportfolio-28315",
-    storageBucket: "personalportfolio-28315.appspot.com",
-    messagingSenderId: "572931778721",
-    appId: "1:572931778721:web:d43a48d33312e558aba8e0"
-  };
+  apiKey: "AIzaSyB22Zti4wa0b70qOBMHP1re5lMIj7ccwwo",
+  authDomain: "personalportfolio-28315.firebaseapp.com",
+  databaseURL: "https://personalportfolio-28315-default-rtdb.firebaseio.com",
+  projectId: "personalportfolio-28315",
+  storageBucket: "personalportfolio-28315.appspot.com",
+  messagingSenderId: "572931778721",
+  appId: "1:572931778721:web:d43a48d33312e558aba8e0"
+};
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-  const contact=firebase.database();
-  const ref=contact.ref("contactForm");
 
-  function submitForm(e){
-      e.preventDefault();
-      var name=document.getElementById("name").value;
-      var email=document.getElementById("email").value;
-      var msg=document.getElementById("msg").value;
-      
-      contact.push({
-          name:name,
-          email:email,
-          msg:msg
-        });
-        console.log(name,email,msg);
-    }
-  
+var messagesRef = firebase.database().ref('messages');
+
+
+
+function submitForm(e){
+  e.preventDefault();
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var msg = getInputVal('msg');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email:email,
+    msg:msg
+  });
+}
